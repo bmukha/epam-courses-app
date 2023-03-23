d3a9b00fdf46dfaeaa85cd2c452e94e7224a21e8
Bohdan Mukha <by.muha@gmail.com>
2023-03-23 12:20:36 +0200

    Remove irrelevant styles in SearchBar component

--- src/components/Courses/components/SearchBar/SearchBar.styles.tsx
+++ src/components/Courses/components/SearchBar/SearchBar.styles.tsx
@@ -2,12 +2,6 @@ import { FlexContainer } from '../../../../common';
 
 import styled from 'styled-components';
 
-const StyledSearchBar = styled(FlexContainer)<FlexContainerProps>`
-	margin: 0 auto;
-
-	& > * {
-		margin: 0 auto;
-	}
-`;
+const StyledSearchBar = styled(FlexContainer)<FlexContainerProps>``;
 
 export default StyledSearchBar;
