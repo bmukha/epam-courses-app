0ff60219c5566b1c727af8f0816d3fa2e644fa7d
Bohdan Mukha <by.muha@gmail.com>
2023-03-27 14:49:36 +0300

    Fix NotFound styles

--- src/common/MainWrapper/MainWrapper.styles.tsx
+++ src/common/MainWrapper/MainWrapper.styles.tsx
@@ -1,6 +1,7 @@
 import { FlexContainer } from '..';
 
 import styled from 'styled-components';
+import StyledNotFound from '../../components/NotFound/NotFound.styles';
 
 const StyledMainWrapper = styled(FlexContainer)<FlexContainerProps>`
 	border-bottom-left-radius: 0;
@@ -10,7 +11,7 @@ const StyledMainWrapper = styled(FlexContainer)<FlexContainerProps>`
 		width: 100%;
 	}
 
-	&{NotFound} {
+	&:has(${StyledNotFound}) {
 		justify-content: center;
 	}
 `;
